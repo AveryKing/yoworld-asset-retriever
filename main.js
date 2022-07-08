7b8a415cdf1435acede69e3eff6c51ffd63d26d4
Avery King <avery.n.king@wmich.edu>
2022-07-07 22:00:00 -0400

    items added to array

--- main.js
+++ main.js
@@ -6,7 +6,20 @@ let con = mysql.createConnection({
     password:''
 });
 
+const items = [];
+
 con.connect(err => {
     if (err) throw err;
-    console.log("Connected!");
+    console.log("Connected to db");
+
+    let sql = "SELECT item_id, filename from mylife_main.items";
+
+    con.query(sql, (err, res) => {
+        if(err) throw err;
+
+        for(let item of res) {
+            items.push({itemId:item.item_id, filename:item.filename});
+        }
+
+    })
 })
